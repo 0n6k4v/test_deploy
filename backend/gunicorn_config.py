9deac61be4afe1d24c9448729d8c46a542064b67
0n6k4v-Coder <kawee26code@gmail.com>
2025-04-30 14:35:48 +0700

    new deploy

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,7 +1,18 @@
 import multiprocessing
+import os
 
-bind = "0.0.0.0:8000"
+# Server socket
+bind = "0.0.0.0:" + str(os.getenv("PORT", "8000"))
+
+# Worker processes
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+
+# Timeout
 timeout = 120
 keepalive = 5 
